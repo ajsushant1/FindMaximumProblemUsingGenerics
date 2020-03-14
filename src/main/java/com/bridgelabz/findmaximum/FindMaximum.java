db34e4282491b0d68f6f07b0531f71711e4f8bfa
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-03-14 12:04:24 +0530

    ADDED: Welcome message

---
+++ src/main/java/com/bridgelabz/findmaximum/FindMaximum.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.findmaximum;
+
+public class FindMaximum {
+    public static void main(String[] args) {
+        System.out.println("/**************************/ WELCOME TO FIND MAXIMUM /**************************/");
+    }
+}
