e629d0974157f9892e1f09347225782c268a016c
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-03-14 14:52:59 +0530

    REFACTORED: getMaximum method and variable name

--- src/main/java/com/bridgelabz/findmaximum/FindMaximum.java
+++ src/main/java/com/bridgelabz/findmaximum/FindMaximum.java
@@ -4,13 +4,14 @@ public class FindMaximum {
 
     //METHOD TO GET MAXIMUM INTEGER NUMBER
     public Integer getMaximumInteger(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
-        Integer max = firstNumber;
-        if (secondNumber.compareTo(max) > 0) {
-            return secondNumber;
-        } else if (thirdNumber.compareTo(max) > 0) {
-            return thirdNumber;
+        Integer maxNumber = firstNumber;
+        if (secondNumber.compareTo(maxNumber) > 0 ) {
+            maxNumber=secondNumber;
         }
-        return firstNumber;
+        if (thirdNumber.compareTo(maxNumber) > 0) {
+            maxNumber=thirdNumber;
+        }
+        return maxNumber;
     }
 
     public static void main(String[] args) {
