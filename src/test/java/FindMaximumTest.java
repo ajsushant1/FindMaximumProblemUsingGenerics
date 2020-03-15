7d7c793118df9f28c2b6701a95762f82ad09e6ff
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-03-15 15:20:35 +0530

    REFACTORED: All test case method to test generic method

--- src/test/java/FindMaximumTest.java
+++ src/test/java/FindMaximumTest.java
@@ -7,85 +7,85 @@ public class FindMaximumTest {
 
     @Test
     public void givenThreeIntegers_WhenMaximumAtFirst_ThenReturnMaximum() {
-        Integer maximumInteger = findMaximum.getMaximumInteger(10, 8, 9);
+        Comparable maximumInteger = findMaximum.getMaximumValue(10, 8, 9);
         Assert.assertEquals((Integer) 10, maximumInteger);
     }
 
     @Test
     public void givenThreeIntegers_WhenMaximumAtMiddle_ThenReturnMaximum() {
-        Integer maximumInteger = findMaximum.getMaximumInteger(10, 18, 9);
+        Comparable maximumInteger = findMaximum.getMaximumValue(10, 18, 9);
         Assert.assertEquals((Integer) 18, maximumInteger);
     }
 
     @Test
     public void givenThreeIntegers_WhenMaximumAtLast_ThenReturnMaximum() {
-        Integer maximumInteger = findMaximum.getMaximumInteger(10, 18, 29);
+        Comparable maximumInteger = findMaximum.getMaximumValue(10, 18, 29);
         Assert.assertEquals((Integer) 29, maximumInteger);
     }
 
     @Test
     public void givenThreeFloats_WhenMaximumAtFirst_ThenReturnMaximum() {
-        Float maximumFloat = findMaximum.getMaximumFloat(22.5f, 8.5f, 12.3f);
+        Comparable maximumFloat = findMaximum.getMaximumValue(22.5f, 8.5f, 12.3f);
         Assert.assertEquals((Float) 22.5f, maximumFloat);
     }
 
     @Test
     public void givenThreeFloats_WhenMaximumAtMiddle_ThenReturnMaximum() {
-        Float maximumFloat = findMaximum.getMaximumFloat(2.5f, 18.5f, 12.3f);
+        Comparable maximumFloat = findMaximum.getMaximumValue(2.5f, 18.5f, 12.3f);
         Assert.assertEquals((Float) 18.5f, maximumFloat);
     }
 
     @Test
     public void givenThreeFloats_WhenMaximumAtLast_ThenReturnMaximum() {
-        Float maximumFloat = findMaximum.getMaximumFloat(2.5f, 18.5f, 32.3f);
+        Comparable maximumFloat = findMaximum.getMaximumValue(2.5f, 18.5f, 32.3f);
         Assert.assertEquals((Float) 32.3f, maximumFloat);
     }
 
     @Test
     public void givenThreeStringsWithLowerCase_WhenMaximumAtFirst_ThenReturnMaximum() {
-        String maximumString = findMaximum.getMaximumString("cat", "bat", "apple");
+        Comparable maximumString = findMaximum.getMaximumValue("cat", "bat", "apple");
         Assert.assertEquals("cat", maximumString);
     }
 
     @Test
     public void givenThreeStringsWithLowerCase_WhenMaximumAtMiddle_ThenReturnMaximum() {
-        String maximumString = findMaximum.getMaximumString("bat", "cat", "apple");
+        Comparable maximumString = findMaximum.getMaximumValue("bat", "cat", "apple");
         Assert.assertEquals("cat", maximumString);
     }
 
     @Test
     public void givenThreeStringsWithLowerCase_WhenMaximumAtLast_ThenReturnMaximum() {
-        String maximumString = findMaximum.getMaximumString("apple", "bat", "cat");
+        Comparable maximumString = findMaximum.getMaximumValue("apple", "bat", "cat");
         Assert.assertEquals("cat", maximumString);
     }
 
     @Test
     public void givenThreeStringsWithUpperCase_WhenMaximumAtFirst_ThenReturnMaximum() {
-        String maximumString = findMaximum.getMaximumString("CAT", "BAT", "APPLE");
+        Comparable maximumString = findMaximum.getMaximumValue("CAT", "BAT", "APPLE");
         Assert.assertEquals("CAT", maximumString);
     }
 
     @Test
     public void givenThreeStringsWithUpperCase_WhenMaximumAtMiddle_ThenReturnMaximum() {
-        String maximumString = findMaximum.getMaximumString("BAT", "CAT", "APPLE");
+        Comparable maximumString = findMaximum.getMaximumValue("BAT", "CAT", "APPLE");
         Assert.assertEquals("CAT", maximumString);
     }
 
     @Test
     public void givenThreeStringsWithUpperCase_WhenMaximumAtLast_ThenReturnMaximum() {
-        String maximumString = findMaximum.getMaximumString("APPLE", "BAT", "CAT");
+        Comparable maximumString = findMaximum.getMaximumValue("APPLE", "BAT", "CAT");
         Assert.assertEquals("CAT", maximumString);
     }
 
     @Test
     public void givenThreeStringsWithUpperCaseAndLowerCase_CompareString_ThenReturnMaximumString() {
-        String maximumString = findMaximum.getMaximumString("APPLE", "bat", "CAT");
+        Comparable maximumString = findMaximum.getMaximumValue("APPLE", "bat", "CAT");
         Assert.assertEquals("bat", maximumString);
     }
 
     @Test
     public void givenThreeNumberAsString_whenCompare_ThenReturnMaximumString() {
-        String maximumString = findMaximum.getMaximumString("5", "8", "3");
+        Comparable maximumString = findMaximum.getMaximumValue("5", "8", "3");
         Assert.assertEquals("8", maximumString);
     }
 
